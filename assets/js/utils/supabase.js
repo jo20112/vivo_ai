3c444208777bd53297af94c648e730ed282e7ac4
jo20112 <135436482+jo20112@users.noreply.github.com>
2025-03-29 00:43:50 +0200

    Update supabase.js

--- assets/js/utils/supabase.js
+++ assets/js/utils/supabase.js
@@ -1,8 +1,6 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://hbvhkmwfquxbwfmccemb.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhidmhrbXdmcXV4YndmbWNjZW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxODI2NTEsImV4cCI6MjA1ODc1ODY1MX0.-yOyr7mQ9Y0S5A1qfDXXPWeZ8ARxqkc6U6UWYeG9uz0';
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
+export const supabase = createClient(supabaseUrl, supabaseKey);
